5260f7af3cd53a974242ac0e33f1be8c080b853e
musharruf <mdmusharruf13@gmail.com>
2024-11-12 21:13:11 +0530

    basic of typescript - number, boolean

---
+++ typescript-fcc_hc/1.basic-types/app.js
@@ -0,0 +1,6 @@
+var num = 98;
+var isActive = true;
+console.log(num, isActive);
+var num2 = 58;
+var isActiveUser = false;
+console.log(num2, isActiveUser);
---
+++ typescript-fcc_hc/1.basic-types/app.ts
@@ -0,0 +1,15 @@
+const num: number = 98;
+const num2 = 598; // this is better than the 'num'
+
+const isActive: boolean = true;
+
+console.log(num, isActive);
+
+type Num = number;
+type Bool = boolean;
+
+const num3: Num = 58;
+
+const isActiveUser: Bool = false;
+
+console.log(num3, isActiveUser);
