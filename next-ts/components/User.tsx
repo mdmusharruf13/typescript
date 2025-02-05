7a29fceec9494cdcc3361bf029dd2b1a0d6d96cf
musharruf <mdmusharruf13@gmail.com>
2025-02-05 07:38:11 +0530

    children prop types

--- next-ts/components/User.tsx
+++ next-ts/components/User.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 type UserProps = {
   name: string;
   age: number;
   isStudent: boolean;
+};
+
+type StudentProps = {
+  children: ReactNode;
 };
 
 function User(props: UserProps) {
@@ -12,6 +16,15 @@ function User(props: UserProps) {
       <h2>{props.name}</h2>
       <h2>{props.age}</h2>
       <h2>{props.isStudent.toString()}</h2>
+    </section>
+  );
+}
+
+export function Student({ children }: StudentProps) {
+  return (
+    <section>
+      {children}
+      <p>I am Learning Next.js</p>
     </section>
   );
 }
--- next-ts/src/app/page.tsx
+++ next-ts/src/app/page.tsx
@@ -1,9 +1,12 @@
-import User from "../../components/User";
+import User, { Student } from "../../components/User";
 
 export default function Home() {
   return (
     <section>
       <User name="Musharruf" age={20} isStudent={true} />
+      <Student>
+        <p>I am Next.js Developer</p>
+      </Student>
     </section>
   );
 }
